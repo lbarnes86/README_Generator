aba9b8783119698090088fab31811fa374caba90
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-11-18 23:04:13 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+
+];
+let dataObj = {title:"this test"};
+
+dataObj.test = "another one"
+
+console.log(generateMarkdown(dataObj));
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
